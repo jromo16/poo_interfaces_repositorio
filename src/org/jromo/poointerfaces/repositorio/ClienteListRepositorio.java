a89fd778f9aa5b6478fc24b108a88e9ec3dc425c
Jhon Romo <jcrr1611@gmail.com>
2026-04-16 21:38:31 -0500

    210. Implementando metodos de interfaces para ordenar y paginar

--- src/org/jromo/poointerfaces/repositorio/ClienteListRepositorio.java
+++ src/org/jromo/poointerfaces/repositorio/ClienteListRepositorio.java
@@ -3,6 +3,7 @@ package org.jromo.poointerfaces.repositorio;
 import org.jromo.poointerfaces.modelo.Cliente;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ClienteListRepositorio implements CrudRepositorio, OrdenableRepositorio, PaginableRepositorio{
@@ -52,11 +53,35 @@ public class ClienteListRepositorio implements CrudRepositorio, OrdenableReposit
 
     @Override
     public List<Cliente> listar(String campo, Direccion dir) {
-        return List.of();
+        dataSource.sort(( a,  b) ->{
+                int resultado = 0;
+                if(dir == Direccion.ASC){
+                    switch (campo){
+                        case "id" ->
+                                resultado = a.getId().compareTo(b.getId());
+                        case "nombre" ->
+                                resultado = a.getNombre().compareTo(b.getNombre());
+                        case "apellido" ->
+                                resultado = a.getApellido().compareTo(b.getApellido());
+                    }
+                } else if(dir == Direccion.DESC){
+                    switch (campo){
+                        case "id" ->
+                                resultado = b.getId().compareTo(a.getId());
+                        case "nombre" ->
+                                resultado = b.getNombre().compareTo(a.getNombre());
+                        case "apellido" ->
+                                resultado = b.getApellido().compareTo(a.getApellido());
+                    }
+                }
+                return resultado;
+            }
+        );
+        return dataSource;
     }
 
     @Override
     public List<Cliente> listar(int desde, int hasta) {
-        return List.of();
+        return dataSource.subList(desde, hasta);
     }
 }
